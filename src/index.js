ac159e7105e932283c96377cfbc6014ab8e484e2
Enrico Pizzol <enrico.pizzol@gmail.com>
2022-04-02 19:35:54 -0300

    add tela login com nova palheta de cores

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Login } from "./components/Login";
 import "./index.css";
 import ShopView from "./views/Shopview";
 ReactDOM.render(
   <React.StrictMode>
-    <ShopView/>
+    <Login/>
   </React.StrictMode>,
   document.getElementById("root")
 );
