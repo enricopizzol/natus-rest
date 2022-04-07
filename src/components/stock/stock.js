439967a903784b29abbaafe5f5fa4301b63c31e3
Enzo Bonacina <epbonacina@gmail.com>
2022-04-07 12:46:06 -0300

    primeiros passos na view do estoque

---
+++ src/components/stock/stock.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { avalaibleProducts } from "../productList/products";
+import {
+  faCircle,
+  faCheckCircle,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import "./stock.css";
+
+
+const Stock = () => {
+
+    const [items, setItems] = useState(avalaibleProducts());
+
+    return (
+        <>  
+            <dic className = "stock-container">
+                <div className = "stock-title">
+                    <h>Estoque</h>
+                </div>
+                <div className="stock-item-container">
+                    {items.map((item, index) => (
+                            <div className = "stock-item-data">
+                                <div className ="stock-itemName">
+                                    <h className = "stock-h">{item.itemName}</h>
+                                </div>
+                                <p className = "stock-p">Quantidade:{item.quantity}</p>
+                                <p className = "stock-p">Preço: R${item.price}</p>
+                            </div> 
+                    ))}
+                </div>
+            </dic>
+        </>
+    )
+}
+
+export default Stock
---
+++ src/views/StockView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./stockView.css";
+import Stock from "../components/stock/stock";
+
+const StockView = () => {
+  return (
+    <>
+        <Stock/>
+    </>
+  );
+};
+
+
+export default StockView;
