2669e29e0240ddf2d8d70bf5fde7725e3816e357
Enzo Bonacina <epbonacina@gmail.com>
2022-04-12 16:48:30 -0300

    primeiros passos no stock

--- src/components/stock/stock.js
+++ src/components/stock/stock.js
@@ -5,6 +5,8 @@ import {
   faCircle,
   faCheckCircle,
   faCheck,
+  faChevronLeft,
+  faChevronRight
 } from "@fortawesome/free-solid-svg-icons";
 import "./stock.css";
 
@@ -13,9 +15,50 @@ const Stock = () => {
 
     const [items, setItems] = useState(avalaibleProducts());
 
+    const handleQuantityIncrease = (index) => {
+      const newItems = [...items];
+  
+      newItems[index].quantity++;
+  
+      setItems(newItems);
+    };
+
+    const handleQuantityDecrease = (index) => {
+      const newItems = [...items];
+  
+      if(newItems[index].quantity > 0)
+        newItems[index].quantity--;
+  
+      setItems(newItems);
+    };
+
+    const handleAddProduct = () => {
+
+      const itemsCopy = [...items];
+      var prodName = prompt("Nome do Produto", "Insira o nome do produto")
+      var prodPrice = prompt("Preço", "0")
+      var prodQtt = prompt("Quantidade em estoque", "Insira a quantidade em estoque")
+      var newItem = {
+          itemName: prodName,
+          quantity: parseInt(prodQtt),
+          price: prodPrice,
+          isSelected: false
+        }
+
+      itemsCopy.push(newItem)
+      setItems(itemsCopy)
+    }
+
+    const handleItemDelete = (index) => {
+      const itemsCopy = [...items];
+      itemsCopy.splice(index,1)
+      setItems(itemsCopy)
+
+    }
+
     return (
         <>  
-            <dic className = "stock-container">
+            <div className = "stock-container">
                 <div className = "stock-title">
                     <h>Estoque</h>
                 </div>
@@ -27,10 +70,16 @@ const Stock = () => {
                                 </div>
                                 <p className = "stock-p">Quantidade:{item.quantity}</p>
                                 <p className = "stock-p">Preço: R${item.price}</p>
+                                <div className = "stock-item-buttons">
+                                  <button onClick = {() => handleQuantityIncrease(index)} className = "stock-p">+</button>
+                                  <button onClick = {() => handleQuantityDecrease(index)} className = "stock-p">-</button>
+                                  <button onClick = {() => handleItemDelete(index)} className = "stock-p">Delete</button>
+                                </div>
                             </div> 
                     ))}
                 </div>
-            </dic>
+                <button onClick = {() => handleAddProduct()} className = "stock-p">Novo item</button>
+            </div>
         </>
     )
 }
