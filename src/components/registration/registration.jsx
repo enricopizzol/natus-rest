013f4752f86df90aee5d03d388d414b1f396696f
Enzo Pedro Bonacina <epbonacina@gmail.com>
2022-04-26 16:34:25 -0300

    primeiros passos da tela de novos registros

---
+++ src/components/registration/registration.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Content,
+  InputContent,
+  Input,
+  StyledLink
+} from "./registration.js";
+import { FaFingerprint, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
+
+export const Registration = () => {
+  const [typePassword, setTypePassword] = useState("password");
+  const [userId, setID] = useState();
+  const [password, setPasword] = useState();
+
+  const changeTypePassword = () => {
+    if (typePassword === "password") {
+      setTypePassword("text");
+    } else {
+      setTypePassword("password");
+    }
+  };
+
+  const registerLoginAndRedirect = (userId, password) => {
+      //Falta implementar a parte de criação de novos registros e de login
+      window.location.replace("/shopview")
+  };
+  
+  return (
+    <Container>
+      <Content>
+        <InputContent>
+          <FaFingerprint />
+          <Input
+            type="text"
+            placeholder="ID"
+            value={userId}
+            onChange={(e) => setID(e.target.value)}
+          />
+        </InputContent>
+        <InputContent>
+          <FaLock />
+          <Input
+            type={typePassword}
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPasword(e.target.value)}
+          />
+
+          {typePassword === "password" ? (
+            <FaEye onClick={changeTypePassword} />
+          ) : (
+            <FaEyeSlash onClick={changeTypePassword} />
+          )}
+        </InputContent>
+        <StyledLink onClick= {() => registerLoginAndRedirect(userId, password)} to = "" disabled={!userId || !password}>
+          Registrar
+        </StyledLink>
+      </Content>
+    </Container>
+  );
+};
+
+export default Registration
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import { Login } from "./components/Login";
+import Registration from "./components/registration/registration.jsx";
 import "./index.css";
 import ShopView from "./views/Shopview";
 import StockView from "./views/StockView";
@@ -12,6 +13,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path = "/" element={<Login />} />
+        <Route path = "/registration" element={<Registration />}/>
         <Route path = "/shopview" index element={< ShopView />} />
         <Route path = "/stockview" index element={< StockView />} />
       </Routes>
