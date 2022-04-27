b7e3728e1f8af4a7bf872341ea0f74f1d9d0d8ec
Enzo Pedro Bonacina <epbonacina@gmail.com>
2022-04-27 19:39:23 -0300

    bug removido

--- src/components/orderHistory/orderHistory.js
+++ src/components/orderHistory/orderHistory.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+import { OrderHistoryExample } from "./orderHistoryExample";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { avalaibleProducts } from "../productList/products";
 import {
   faChevronRight,
   faChevronLeft,
@@ -10,45 +10,7 @@ import {
 import "./orderHistory.css";
 
 const OrderHistory = () => {
-  const [items, setItems] = useState(avalaibleProducts());
-  const [userHistory, setUserHistory] = useState("{}")
-
-  const _requestUserOrderHistory = (userId) => {
-    return(
-        `{
-            "orders": [
-                {"products": [
-                    {"name":"Pão de Queijo", "quantity": 10, "price": 3.0},
-                    {"name":"Todinho", "quantity": 4, "price": 1.5},
-                    {"name":"7Bello", "quantity": 8, "price": 0.5},
-                ]},
-                {"products": [
-                    {"name":"Cachorro Quente", "quantity": 1, "price": 8.0},
-                    {"name":"Coca-Cola", "quantity": 1, "price": 5.5},
-                    {"name":"Pastel", "quantity": 2, "price": 7.5},
-                ]},
-                {"products": [
-                    {"name":"Banana", "quantity": 4, "price": 4.75},
-                    {"name":"Água (600mL)", "quantity": 1, "price": 1.5},
-                ]},
-            ]
-        }`
-    )
-  };
-
-  const getUserOrderHistory = (userId) => {
-    history = _requestUserOrderHistory(userId)
-    setUserHistory(JSON.parse(history))
-  };
-
-  const handleQuantityIncrease = (index) => {
-    const newItems = [...items];
-
-    newItems[index].quantity++;
-
-    setItems(newItems);
-    calculateBill();
-  };
+  const [userHistory, setUserHistory] = useState(OrderHistoryExample());
 
   return (
     <>
