c98689290bca9c3d53831b9ce5c618d9e36c3431
Enzo Pedro Bonacina <epbonacina@gmail.com>
2022-04-28 18:49:59 -0300

    estrutura ok, falta estilizar

--- src/components/orderHistory/orderHistory.js
+++ src/components/orderHistory/orderHistory.js
@@ -10,32 +10,36 @@ import {
 import "./orderHistory.css";
 
 const OrderHistory = () => {
-  const [userHistory, setUserHistory] = useState(OrderHistoryExample());
+    const [userHistory, setUserHistory] = useState(JSON.parse(OrderHistoryExample()));
 
-  return (
-    <>
-        <div className="hs-container">
-            {userHistory.map((order, orderIndex) => (
-                <table className="hs-order-container">
-                    <tr>
-                        <th>Produto</th>
-                        <th>Quantidade</th>
-                        <th>Preço</th>
-                    </tr>
-                    <tr>
-                        {order.map((product, productIndex) => (
-                            <>
-                                <td className = "hs-product-data">{product.name}</td>
-                                <td className = "hs-product-data">{product.quantity}</td>
-                                <td className = "hs-product-data">{product.price}</td>
-                            </>
-                        ))}
-                    </tr>
-                </table>
-            ))}
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className="hs-container">
+                {userHistory["orders"].map((order, orderIndex) => (
+                    <>
+                        <table className="hs-order-container">
+                            <tbody>
+                                <tr className = "hs">
+                                    <th className = "hs">Produto</th>
+                                    <th className = "hs">Quantidade</th>
+                                    <th className = "hs">Preço</th>
+                                </tr>
+                                {order["products"].map((product, productIndex) => (
+                                    <tr className = "hs">
+                                        <td className = "hs">{product.name}</td>
+                                        <td className = "hs">{product.quantity}</td>
+                                        <td className = "hs">{product.price}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <span>Total do pedido: {order["total"]}</span>
+                        <span>Data: {order["date"]}</span>
+                    </>
+                ))}
+            </div>
+        </>
+    )
 };
 
 export default OrderHistory;
