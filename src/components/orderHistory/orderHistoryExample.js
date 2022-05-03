fb01be65e71cf9d0b73bc7af65dbaf112b5a240e
Enzo <epbonacina@gmail.com>
2022-05-02 21:59:10 -0300

    fix tabela

--- src/components/orderHistory/orderHistoryExample.js
+++ src/components/orderHistory/orderHistoryExample.js
@@ -4,39 +4,12 @@ export const OrderHistoryExample = () => {
             "orders": [
                 {
                     "products": [
-                        { "name": "Sanduba", "quantity": 10, "price": 10.0, "category": "Salgado" },
-                        { "name": "Baguete", "quantity": 20, "price": 5, "category": "Salgado"},
-                        { "name": "Coca - Cola", "quantity": 30, "price": 5.5, "category": "Bebida"}
+                        { "name": "Sanduba", "quantity": 2, "price": 10.0, "category": "Salgado" },
+                        { "name": "Baguete", "quantity": 2, "price": 5, "category": "Salgado"},
+                        { "name": "Coca - Cola", "quantity": 1, "price": 5.5, "category": "Bebida"}
                     ],
                     "total": 365.0,
                     "date": "22/04/2022"
-                },
-                {
-                    "products": [
-                        { "name": "Hamburguer", "quantity": 25, "price": 10.0, "category": "Salgado"},
-                        { "name": "Enroladinho", "quantity": 15, "price": 5.0, "category": "Salgado"},
-                        { "name": "Fanta", "quantity": 40, "price": 5.5, "category": "Bebida"}
-                    ],
-                    "total": 545.0,
-                    "date": "18/03/2022"
-                },
-                {
-                    "products": [
-                        { "name": "Salada de fruta", "quantity": 5, "price": 5.5, "category": "Bebida" },
-                        { "name": "Mousse de chocolate", "quantity": 5, "price": 5.5, "category": "Doce" },
-                        { "name": "Pudim", "quantity": 8, "price": 6.8, "category": "Doce" },
-                        { "name": "Toddynho", "quantity": 12, "price": 6.8, "category": "Bebida" },
-                    ],
-                    "total": 20.5,
-                    "date": "12/01/2022"
-                },
-                {
-                    "products": [
-                        { "name": "Banana", "quantity": 20, "price": 6.8, "category": "Saudável" },
-                        { "name": "H2O", "quantity": 40, "price": 6.8, "category": "Bebida" }
-                    ],
-                    "total": 20.5,
-                    "date": "12/01/2022"
                 }
             ]
         }`
